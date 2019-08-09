d040086881f6fbce13155fe0cccc8361562e7528
mittulmandhan <conanmittul@gmail.com>
2019-08-10 04:38:40 +0530

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'relative-navigation'`, () => {
+  it(`should have as title 'routing-and-navigation'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('relative-navigation');
+    expect(app.title).toEqual('routing-and-navigation');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to relative-navigation!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to routing-and-navigation!');
   });
 });
